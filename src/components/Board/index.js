761eeb111be8273289bad250a37849a309156565
Jurbas <jurbasgames@gmail.com>
2022-04-03 23:21:36 -0300

    feature: Letter component

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { boardDefault } from "../../Words";
+import Letter from "../Letter"
 
 function Board() {
   const [board, setBoard] = useState(boardDefault);
   return (
     <div className="board">
-      <div className="row"></div>
+      <div className="row">
+        <Letter letterPos={1} attempVal={1}/>
+        <Letter letterPos={1} attempVal={1}/>
+        <Letter letterPos={1} attempVal={1}/>
+        <Letter letterPos={1} attempVal={1}/>
+        <Letter letterPos={1} attempVal={1}/>
+      </div>
       <div className="row"></div>
       <div className="row"></div>
       <div className="row"></div>
@@ -13,6 +20,6 @@ function Board() {
       <div className="row"></div>
     </div>
   );
-}
+};
 
 export default Board;
