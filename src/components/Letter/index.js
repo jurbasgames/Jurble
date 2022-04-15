437d84a2fa3d6dbc1f6058de370da22e952bfe84
Jurbas <jurbasgames@gmail.com>
2022-04-14 22:00:07 -0300

    feature: keyboard input

--- src/components/Letter/index.js
+++ src/components/Letter/index.js
@@ -2,10 +2,20 @@ import React, { useContext } from "react";
 import { AppContext } from "../../App";
 
 function Letter({ letterPos, attemptVal }) {
-  const { board } = useContext(AppContext);
+  const { board, solution, currAttempt } = useContext(AppContext);
   const letter = board[attemptVal][letterPos];
 
-  return <div className="letter"> {letter} </div>;
+  const correct = solution[letterPos] === letter;
+  const almost = !correct && letter !== "" && solution.includes(letter);
+
+  const letterState = currAttempt.attempt > attemptVal && (correct ? "correct" : almost ? "almost" : "error");
+
+  return (
+    <div id={letterState} className="letter">
+      {" "}
+      {letter}{" "}
+    </div>
+  );
 }
 
 export default Letter;
